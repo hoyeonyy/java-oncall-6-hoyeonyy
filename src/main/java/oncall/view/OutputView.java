806f5c3d94d85221d5b3acf1dda07f61343d5348
hoyeon <highoption@naver.com>
2023-12-16 18:24:54 +0900

    style(OutputView) : 공백추가

--- src/main/java/oncall/view/OutputView.java
+++ src/main/java/oncall/view/OutputView.java
@@ -18,7 +18,7 @@ public class OutputView {
         for (Day day1 : result) {
             String holi = " ";
             if (day1.getHoliday()) {
-                holi = "(휴일)";
+                holi = "(휴일) ";
             }
             System.out.printf(Message.OUTPUT_GAME_RESULT.message,
                     month, day1.getDay(), day1.getSevenDay(), holi, day1.getName());
