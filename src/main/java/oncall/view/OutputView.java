0a80b67c4927c9496046fc29f35522a1255721c2
hoyeon <highoption@naver.com>
2023-12-16 18:07:55 +0900

    style(OuputView) : printResult 메서드 수정

--- src/main/java/oncall/view/OutputView.java
+++ src/main/java/oncall/view/OutputView.java
@@ -15,10 +15,13 @@ public class OutputView {
     }
 
     public static void printResult(List<Day> result, int month) {
-        result.forEach(day1 -> {
-            String holi = day1.getHoliday() ? "(휴일)" : " ";
-            System.out.printf(Message.OUTPUT_GAME_RESULT.message, month, day1.getDay(), day1.getSevenDay(), holi,
-                    day1.getName());
-        });
+        for (Day day1 : result) {
+            String holi = " ";
+            if (day1.getHoliday()) {
+                holi = "(휴일)";
+            }
+            System.out.printf(Message.OUTPUT_GAME_RESULT.message,
+                    month, day1.getDay(), day1.getSevenDay(), holi, day1.getName());
+        }
     }
 }
