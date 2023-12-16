5c4726a9ef6b5ce0e11953dc0d88ac9ad10ac679
hoyeon <highoption@naver.com>
2023-12-16 15:40:09 +0900

    feat : 각 날짜의 사람과, 요일, 휴일을 저장할 Day 생성

---
+++ src/main/java/oncall/model/Day.java
@@ -0,0 +1,45 @@
+package oncall.model;
+
+public class Day {
+    private int day;
+    private String sevenDay;
+    private boolean holiday;
+    private String name;
+
+    public Day(int day, String sevenDay, boolean holiday) {
+        this.day = day;
+        this.sevenDay = sevenDay;
+        this.holiday = holiday;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public String getSevenDay() {
+        return sevenDay;
+    }
+
+    public boolean getHoliday() {
+        return holiday;
+    }
+
+    public boolean isWeekend() {
+        if (sevenDay.equals("토") || sevenDay.equals("일") || holiday == true) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isWeekday() {
+        if (holiday == false && (sevenDay.equals("월") || sevenDay.equals("화") || sevenDay.equals("수")
+                || sevenDay.equals("목") || sevenDay.equals("금"))) {
+            return true;
+        }
+        return false;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
