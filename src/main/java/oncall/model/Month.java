66c9f01862fc67a7644616866314c2d74919b2d8
hoyeon <highoption@naver.com>
2023-12-16 15:40:28 +0900

    feat : Day를 가지고 있는 Month 생성

--- src/main/java/oncall/model/Month.java
+++ src/main/java/oncall/model/Month.java
@@ -4,6 +4,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Month {
+    private List<Day> monthDatabase;
+
+    public List<Day> getMonthDatabase() {
+        return monthDatabase;
+    }
+
+    public Month(int month, String day) {
+        List<Day> db = new ArrayList<>();
+
+        boolean holidayChecked = false;
+        for (int i = 1; i <= makeMonthCount(month); i++) {
+            List<Integer> holiday = makeHoliday(month);
+            if (holiday.contains(i)) {
+                holidayChecked = true;
+            }
+            Day newDay = new Day(i, day, holidayChecked);
+            holidayChecked = false;
+            day = nextDay(day);
+            db.add(newDay);
+        }
+        monthDatabase = db;
+    }
+
+    public String nextDay(String day) {
+        if (day.equals("월")) {
+            return "화";
+        }
+        if (day.equals("화")) {
+            return "수";
+        }
+        if (day.equals("수")) {
+            return "목";
+        }
+        if (day.equals("목")) {
+            return "금";
+        }
+        if (day.equals("금")) {
+            return "토";
+        }
+        if (day.equals("토")) {
+            return "일";
+        }
+        if (day.equals("일")) {
+            return "월";
+        }
+        return "";
+    }
+
     public List<Integer> makeHoliday(int month) {
         List<Integer> holiday = new ArrayList<>();
         if (month == 1) {
@@ -29,5 +77,18 @@ public class Month {
             holiday.add(25);
         }
         return holiday;
+    }
+
+    public int makeMonthCount(int month) {
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+            return 31;
+        }
+        if (month == 4 || month == 6 || month == 9 || month == 11) {
+            return 30;
+        }
+        if (month == 2) {
+            return 28;
+        }
+        return 0;
     }
 }
